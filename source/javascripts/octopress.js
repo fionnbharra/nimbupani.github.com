9674901d6dd5018967e13395555ba8e8d78fe580
Divya Manian <divya.manian@gmail.com>
2011-12-10 19:19:29 -0800

    updating source

--- source/javascripts/octopress.js
+++ source/javascripts/octopress.js
@@ -1,64 +1,3 @@
-function getNav() {
-  var mobileNav = $('nav[role=navigation] fieldset[role=search]').after('<fieldset class="mobile-nav"></fieldset>').next().append('<select></select>');
-  mobileNav.children('select').append('<option value="">Navigate&hellip;</option>');
-  $('ul[role=main-navigation]').addClass('main-navigation');
-  $('ul.main-navigation a').each(function(link) {
-    mobileNav.children('select').append('<option value="'+link.href+'">&bull; '+link.text+'</option>');
-  });
-  mobileNav.children('select').bind('change', function(event) {
-    if (event.target.value) { window.location.href = event.target.value; }
-  });
-}
-
-function addSidebarToggler() {
-  if(!$('body').hasClass('sidebar-footer')) {
-    $('#content').append('<span class="toggle-sidebar"></span>');
-    $('.toggle-sidebar').bind('click', function(e) {
-      e.preventDefault();
-      if ($('body').hasClass('collapse-sidebar')) {
-        $('body').removeClass('collapse-sidebar');
-      } else {
-        $('body').addClass('collapse-sidebar');
-      }
-    });
-  }
-  var sections = $('aside.sidebar > section');
-  if (sections.length > 1) {
-    sections.each(function(section, index){
-      if ((sections.length >= 3) && index % 3 === 0) {
-        $(section).addClass("first");
-      }
-      var count = ((index +1) % 2) ? "odd" : "even";
-      $(section).addClass(count);
-    });
-  }
-  if (sections.length >= 3){ $('aside.sidebar').addClass('thirds'); }
-}
-
-
-function addCodeLineNumbers() {
-  if (navigator.appName === 'Microsoft Internet Explorer') { return; }
-  $('div.gist-highlight').each(function(code) {
-    var tableStart = '<table><tbody><tr><td class="gutter">',
-        lineNumbers = '<pre class="line-numbers">',
-        tableMiddle = '</pre></td><td class="code">',
-        tableEnd = '</td></tr></tbody></table>',
-        count = $('.line', code).length;
-    for (var i=1;i<=count; i++) {
-      lineNumbers += '<span class="line-number">'+i+'</span>\n';
-    }
-    var table = tableStart + lineNumbers + tableMiddle + '<pre>'+$('pre', code).html()+'</pre>' + tableEnd;
-    $(code).html(table);
-  });
-}
-
-
-$.domReady(function() {
-  addCodeLineNumbers();
-  getNav();
-  addSidebarToggler();
-});
-
 // iOS scaling bug fix
 // Rewritten version
 // By @mathias, @cheeaun and @jdalton
@@ -67,8 +6,11 @@ $.domReady(function() {
   var addEvent = 'addEventListener',
       type = 'gesturestart',
       qsa = 'querySelectorAll',
+      qs = 'querySelector',
       scales = [1, 1],
-      meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+      meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [],
+      previousLink = qs in doc ? doc[qs]('.article .previous') : null,
+      nextLink = qs in doc ? doc[qs]('.article .next') : null;
   function fix() {
     meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
     doc.removeEventListener(type, fix, true);
@@ -78,4 +20,14 @@ $.domReady(function() {
     scales = [0.25, 1.6];
     doc[addEvent](type, fix, true);
   }
+  
+  window.onkeypress = function(e) {
+    if(e.keyCode == 37 && previousLink) {
+      previousLink.click();
+    }
+    
+    if(e.keyCode == 39 && nextLink) {
+      nextLink.click();
+    }
+  };
 }(document));
